50ab7c9a3c9f5efc78dc1e542c10e7de1d888994
Noah Zakaib <ndz@sfu.ca>
2025-05-19 23:57:20 -0700

    First Commit

---
+++ src/main/java/com/valmatchpredictor/ValMatchPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.valmatchpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValMatchPredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValMatchPredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valmatchpredictor/ValMatchPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valmatchpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValMatchPredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
