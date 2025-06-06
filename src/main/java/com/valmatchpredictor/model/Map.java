51285998ea332e9b7b15527d8a5ab1e51f49edba
Noah Zakaib <ndz@sfu.ca>
2025-06-05 23:36:39 -0700

    Map class created

--- src/main/java/com/valmatchpredictor/model/Map.java
+++ src/main/java/com/valmatchpredictor/model/Map.java
@@ -1,4 +1,49 @@
 package com.valmatchpredictor.model;
 
 public class Map {
+    private String name;
+    private String team1;
+    private String team2;
+    private String score1;
+    private String score2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public String getScore1() {
+        return score1;
+    }
+
+    public void setScore1(String score1) {
+        this.score1 = score1;
+    }
+
+    public String getScore2() {
+        return score2;
+    }
+
+    public void setScore2(String score2) {
+        this.score2 = score2;
+    }
 }
