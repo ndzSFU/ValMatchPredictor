783bf6d60d4c336dafb3f7886218879519c9e54b
Noah Zakaib <ndz@sfu.ca>
2025-10-07 03:09:10 -0700

    Updated dependcies and updated db access in app properties

--- src/main/java/com/valmatchpredictor/model/RankingResponse.java
+++ src/main/java/com/valmatchpredictor/model/RankingResponse.java
@@ -1,35 +1,35 @@
-package com.valmatchpredictor.model;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
-
-public class RankingResponse {
-
-    private int status;
-
-    @JsonProperty("data")
-    private List<Team_Old_API> teams;
-
-    public RankingResponse() {
-    }
-
-    public RankingResponse(int status, List<Team_Old_API> teams) {
-        this.status = status;
-        this.teams = teams;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public List<Team_Old_API> getTeams() {
-        return teams;
-    }
-
-    public void setTeams(List<Team_Old_API> teams) {
-        this.teams = teams;
-    }
-}
+//package com.valmatchpredictor.model;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import java.util.List;
+//
+//public class RankingResponse {
+//
+//    private int status;
+//
+//    @JsonProperty("data")
+//    private List<Team_Old_API> teams;
+//
+//    public RankingResponse() {
+//    }
+//
+//    public RankingResponse(int status, List<Team_Old_API> teams) {
+//        this.status = status;
+//        this.teams = teams;
+//    }
+//
+//    public int getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(int status) {
+//        this.status = status;
+//    }
+//
+//    public List<Team_Old_API> getTeams() {
+//        return teams;
+//    }
+//
+//    public void setTeams(List<Team_Old_API> teams) {
+//        this.teams = teams;
+//    }
+//}
--- src/main/java/com/valmatchpredictor/model/Team_Old_API.java
+++ src/main/java/com/valmatchpredictor/model/Team_Old_API.java
@@ -1,64 +1,64 @@
-package com.valmatchpredictor.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public class Team_Old_API {
-    @JsonProperty("team")
-    private String name;
-    private String rank;
-    private String record;
-    private String logo;
-    private List<Match> matches;
-
-    public List<Match> getMatches() {
-        return matches;
-    }
-
-    public void setMatches(List<Match> matches) {
-        this.matches = matches;
-    }
-
-    public Team_Old_API() {
-    }
-
-    public Team_Old_API(String name, String rank, String record, String logo) {
-        this.name = name;
-        this.rank = rank;
-        this.record = record;
-        this.logo = logo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public String getRecord() {
-        return record;
-    }
-
-    public void setRecord(String record) {
-        this.record = record;
-    }
-
-    public String getLogo() {
-        return logo;
-    }
-
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
-}
+//package com.valmatchpredictor.model;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//import java.util.List;
+//
+//public class Team_Old_API {
+//    @JsonProperty("team")
+//    private String name;
+//    private String rank;
+//    private String record;
+//    private String logo;
+//    private List<Match> matches;
+//
+//    public List<Match> getMatches() {
+//        return matches;
+//    }
+//
+//    public void setMatches(List<Match> matches) {
+//        this.matches = matches;
+//    }
+//
+//    public Team_Old_API() {
+//    }
+//
+//    public Team_Old_API(String name, String rank, String record, String logo) {
+//        this.name = name;
+//        this.rank = rank;
+//        this.record = record;
+//        this.logo = logo;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getRank() {
+//        return rank;
+//    }
+//
+//    public void setRank(String rank) {
+//        this.rank = rank;
+//    }
+//
+//    public String getRecord() {
+//        return record;
+//    }
+//
+//    public void setRecord(String record) {
+//        this.record = record;
+//    }
+//
+//    public String getLogo() {
+//        return logo;
+//    }
+//
+//    public void setLogo(String logo) {
+//        this.logo = logo;
+//    }
+//}
