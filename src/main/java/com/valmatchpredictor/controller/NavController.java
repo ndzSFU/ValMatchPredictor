512cbefb04deb946e058796893b6fc145b1df01a
Noah Zakaib <ndz@sfu.ca>
2026-01-25 21:55:09 -0800

    Fixed reroute error on api

---
+++ src/main/java/com/valmatchpredictor/controller/NavController.java
@@ -0,0 +1,18 @@
+package com.valmatchpredictor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NavController {
+
+    @RequestMapping(value = {
+            "/{path:[^.]*}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+
+}
+
+
